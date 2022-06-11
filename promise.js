d67656506e22a8dc3865986af27834ce9a5759a1
Sopan Setiawan <setiawan.sopan@gmail.com>
2022-06-11 21:36:34 +0700

    feat : kode edit 1

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC, promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (keyword) => {
+  try {
+    const promiseTheaterIXX = await promiseTheaterIXX();
+    const promiseTheaterVGC = await promiseTheaterVGC();
+
+    const data = await[...promiseTheaterIXX, ...theaterVGC ];
+
+    const countMatchedKey = data.filter(item => {}).length;
+    return countMatchedKey;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
